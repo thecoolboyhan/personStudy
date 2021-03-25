20965808743154022ff57ba2594959978d35733f
hanyf <hanyf@i2finance,net>
2021-03-25 09:07:21 +0800

    1.个人提价 2.

---
+++ 多线程/src/线程的三大特性/kejianxing.java
@@ -0,0 +1,23 @@
+package 线程的三大特性;
+
+/**
+ * 韩永发
+ *
+ * @Date 14:01 2021/3/23
+ */
+public class kejianxing {
+
+  public static void main(String[] args) {
+
+  }
+}
+
+class abb extends Thread{
+
+  @Override
+  public void run() {
+    String name = Thread.currentThread().getName();
+    System.out.println("当前线程的名字："+name);
+  }
+
+}
