7a3603053db65b98d5f53ad15d8042da9b28fc64
hanyongfa <hanyongfa2013@163.com>
2021-11-04 10:13:49 +0800

    提交上午的笔记

--- java杂项/src/com/JVM/Test/Test01.java
+++ java杂项/src/com/JVM/Test/Test01.java
@@ -13,6 +13,23 @@ public class Test01 {
   }
   public static int abb=3;
   public static void main(String[] args) {
-    System.out.printf("%d\n%d",Test01.mun,Test01.abb);//mun=2,abb=3
+//    System.out.printf("%d\n%d",Test01.mun,Test01.abb);//mun=2,abb=3S
+    String s1="a";
+    String s2="b";
+    String s3="a"+"b";
+    String s4=s1+s2;
+    String s5="ab";
+    String s6=s4.intern();
+
+    System.out.println(s3 == s4);
+    System.out.println(s3 == s5);
+    System.out.println(s3 == s6);
+
+    String x2=new String("c")+new String("d");
+    String x1="cd";
+    x2.intern();
+
+    System.out.println(x1 == x2);
+
   }
 }
