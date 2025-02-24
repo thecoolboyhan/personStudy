cd83699ecfe660aa11f2872232694c5c5f8e43b1
hyf <hanyongfa2013@163.com>
2025-02-24 16:04:36 +0800

    提交博客，java21核心包虚拟线程相关代码

---
+++ JDK21CoreLibraries/src/Concurrency/VirtualThreads/VirtualThreadGuide.java
@@ -0,0 +1,11 @@
+package Concurrency.VirtualThreads;
+
+import java.util.concurrent.CompletableFuture;
+
+public class VirtualThreadGuide {
+    public static void main(String[] args) {
+
+    }
+
+
+}
